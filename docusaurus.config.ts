8a1704620081fa29b0a3f529aedb38287cc7a96c
gretmn102 <erlgreywatson@gmail.com>
2024-04-05 10:34:50 +0300

    enable mermaid

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -61,6 +61,12 @@ const config: Config = {
     ],
   ],
 
+  themes: ['@docusaurus/theme-mermaid'],
+
+  markdown: {
+    mermaid: true,
+  },
+
   themeConfig: {
     // Replace with your project's social card
     image: 'img/funnysock-social-card.png',
